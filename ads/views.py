fe64e8b5f52131906f25b4209ef980b0082c109b
FMaslina <FMaslina@yandex.ru>
2024-06-24 20:13:39 +0700

    Create AdvertisementViewSet

--- ads/views.py
+++ ads/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from ads.models import Advertisement
+from ads.serializers import AdvertisementSerializer
+
+
+@permission_classes([IsAuthenticated])
+class AdvertisementViewSet(viewsets.ModelViewSet):
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
