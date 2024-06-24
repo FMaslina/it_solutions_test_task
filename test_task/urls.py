ddf5447d9a63bd3e0be644c5928b11055bc8e0b1
FMaslina <FMaslina@yandex.ru>
2024-06-24 20:51:41 +0700

    Add simple-jwt auth

--- test_task/urls.py
+++ test_task/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from ads.views import AdvertisementViewSet
 from users.views import CreateUserView
@@ -28,7 +29,9 @@ router.register(r'ads', AdvertisementViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
+    path('api/v1/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
     path('api/v1/', include(router.urls)),
-    path('api/v1/register/', CreateUserView.as_view(), name='register')
+    path('api/v1/register/', CreateUserView.as_view(), name='register'),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
